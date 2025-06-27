c53a47e639b2b702cf8adcef996be2282305018f
ngdnhtien <ngdnhtien_t65@hus.edu.vn>
2025-06-27 10:44:41 +0200

    clean up 1yr

--- constant.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-
-pi = np.pi
-e = np.e
-h = 6.6261e-34
-hbar = h/(2*pi)
-electron_charge = 1.602e-19
--- elementary_func.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-cos = np.cos
-sin = np.sin
-
-sqrt = np.sqrt
-ln = np.log
-factorial = np.math.factorial
-exp = np.exp
